0409aa080d5e854535e0edf2672f5963b89e1f2d
Shaikh Mahad <codewithmahad+github@gmail.com>
2026-01-05 17:44:31 +0500

    perf: optimize fontawesome css delivery

--- Portfolio/app/layout.js
+++ Portfolio/app/layout.js
@@ -3,6 +3,10 @@ import { Inter, JetBrains_Mono } from 'next/font/google';
 import { SystemProvider } from './components';
 import ClientLayout from './components/ClientLayout';
 import { Analytics } from '@vercel/analytics/react';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 const inter = Inter({
   subsets: ['latin'],
